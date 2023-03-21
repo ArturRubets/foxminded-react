259fabdb47b6cc4ae5a03788f577347bd790373b
ArturRubets <artur.rubets@gmail.com>
2023-03-21 17:10:13 +0200

    Add post lists page

---
+++ task-3.3/src/components/StyledNavbar.js
@@ -0,0 +1,25 @@
+import { NavLink } from 'react-router-dom';
+
+const StyledNavbar = () => {
+  return (
+    <nav className="navbar">
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? 'link active' : 'link')}>
+        PostList
+      </NavLink>
+      <NavLink
+        to="/todo-list"
+        className={({ isActive }) => (isActive ? 'link active' : 'link')}>
+        TodoList
+      </NavLink>
+      <NavLink
+        to="/user-list"
+        className={({ isActive }) => (isActive ? 'link active' : 'link')}>
+        UserList
+      </NavLink>
+    </nav>
+  );
+};
+
+export default StyledNavbar;
