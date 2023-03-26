decdaf7683776783e39f8e9b719b3a8423762551
ArturRubets <artur.rubets@gmail.com>
2023-03-26 19:43:28 +0300

    Change home page

--- task-3.4/src/pages/Home.js
+++ task-3.4/src/pages/Home.js
@@ -2,32 +2,47 @@ import { useSelector } from 'react-redux';
 
 const Home = () => {
   const postsData = useSelector((state) => state.posts.data);
-  const usersData = useSelector((state) => state.users.data);
   const todosData = useSelector((state) => state.todos.data);
+  const usersData = useSelector((state) => state.users.data);
 
   return (
     <section>
       <article>
         <p>PostList</p>
         <ul>
-          {postsData.map((post, index) => (
-            <li key={index}>{post}</li>
+          {postsData.map((post) => (
+            <div key={post.id} className="data">
+              <div className="name">Title</div>
+              <div>{post.title}</div>
+              <div className="name">Body</div>
+              <div>{post.body}</div>
+            </div>
           ))}
         </ul>
       </article>
       <article>
-        <p>TodoList</p>
+        <p>TodosList</p>
         <ul>
-          {todosData.map((todo, index) => (
-            <li key={index}>{todo}</li>
+          {todosData.map((post) => (
+            <div key={post.id} className="data">
+              <div className="name">Title</div>
+              <div>{post.title}</div>
+              <div className="name">Completed</div>
+              <div>{post.completed}</div>
+            </div>
           ))}
         </ul>
       </article>
       <article>
-        <p>UserList</p>
+        <p>UsersList</p>
         <ul>
-          {usersData.map((user, index) => (
-            <li key={index}>{user}</li>
+          {usersData.map((post) => (
+            <div key={post.id} className="data">
+              <div className="name">Name</div>
+              <div>{post.name}</div>
+              <div className="name">Username</div>
+              <div>{post.username}</div>
+            </div>
           ))}
         </ul>
       </article>
