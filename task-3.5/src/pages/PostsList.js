a3a6816b3ec183d1da017f674971b61c88521bdd
ArturRubets <artur.rubets@gmail.com>
2023-03-27 14:52:37 +0300

    Add drag and drop for post list

--- task-3.5/src/pages/PostsList.js
+++ task-3.5/src/pages/PostsList.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { DragDropContext, Draggable } from 'react-beautiful-dnd';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   deletePosts,
   postPosts,
   postsUpdated,
 } from '../features/posts/postsSlice';
+import { StrictModeDroppable as Droppable } from '../helpers/StrictModeDroppable';
 
 const PostsList = () => {
   const [isAdd, setIsAdd] = useState(false);
@@ -61,74 +63,110 @@ const PostsList = () => {
     }
   };
 
+  const onDragEnd = (result) => {
+    if (!result?.destination) return;
+    const items = [...posts];
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setPosts(items);
+  };
+
+  const content = (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="posts">
+        {(provided) => (
+          <section {...provided.droppableProps} ref={provided.innerRef}>
+            {posts.map((item, index) => (
+              <Draggable
+                key={item.id}
+                draggableId={item.id.toString()}
+                index={index}>
+                {(provided) => (
+                  <article
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                    className="excerpt">
+                    <label>
+                      Post Title:
+                      <input
+                        type="text"
+                        value={item.title}
+                        onChange={(e) => onChangeTitle(e, item.id)}
+                      />
+                    </label>
+                    <label>
+                      Content:
+                      <textarea
+                        value={item.body}
+                        onChange={(e) => onChangeBody(e, item.id)}
+                      />
+                    </label>
+                    <button className="btn" onClick={() => onDelete(item.id)}>
+                      Delete
+                    </button>
+                  </article>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </section>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+
+  const newItemSection = isAdd && (
+    <div className="fill-new-post">
+      <hr />
+      <article className="excerpt">
+        <label>
+          Post Title:
+          <input
+            type="text"
+            value={newPost.title}
+            onChange={(e) => {
+              setNewPost({ ...newPost, title: e.target.value });
+            }}
+          />
+        </label>
+        <label>
+          Content:
+          <textarea
+            value={newPost.body}
+            onChange={(e) => {
+              setNewPost({ ...newPost, body: e.target.value });
+            }}
+          />
+        </label>
+      </article>
+    </div>
+  );
+
+  const buttons = (
+    <div className="btn-container">
+      {isAdd && (
+        <button className="btn btn-hipster" onClick={onCancel}>
+          Cancel
+        </button>
+      )}
+      {!isAdd && (
+        <button className="btn" onClick={onAdd}>
+          Add new
+        </button>
+      )}
+      <button className="btn" onClick={onSave}>
+        Save
+      </button>
+    </div>
+  );
+
   return (
     <section className="section posts-list">
       <h2>PostsList</h2>
-      {posts.map((item) => (
-        <React.Fragment key={item.id}>
-          <article className="excerpt">
-            <label>
-              Post Title:
-              <input
-                type="text"
-                value={item.title}
-                onChange={(e) => onChangeTitle(e, item.id)}
-              />
-            </label>
-            <label>
-              Content:
-              <textarea
-                value={item.body}
-                onChange={(e) => onChangeBody(e, item.id)}
-              />
-            </label>
-            <button className="btn" onClick={() => onDelete(item.id)}>
-              Delete
-            </button>
-          </article>
-        </React.Fragment>
-      ))}
-      {isAdd && (
-        <div className="fill-new-post">
-          <hr />
-          <article className="excerpt">
-            <label>
-              Post Title:
-              <input
-                type="text"
-                value={newPost.title}
-                onChange={(e) => {
-                  setNewPost({ ...newPost, title: e.target.value });
-                }}
-              />
-            </label>
-            <label>
-              Content:
-              <textarea
-                value={newPost.body}
-                onChange={(e) => {
-                  setNewPost({ ...newPost, body: e.target.value });
-                }}
-              />
-            </label>
-          </article>
-        </div>
-      )}
-      <div className="btn-container">
-        {isAdd && (
-          <button className="btn btn-hipster" onClick={onCancel}>
-            Cancel
-          </button>
-        )}
-        {!isAdd && (
-          <button className="btn" onClick={onAdd}>
-            Add new
-          </button>
-        )}
-        <button className="btn" onClick={onSave}>
-          Save
-        </button>
-      </div>
+      {content}
+      {newItemSection}
+      {buttons}
     </section>
   );
 };
