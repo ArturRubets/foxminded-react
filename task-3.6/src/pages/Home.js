911da74a9390d14600150c21eb1e89ebf40717f5
ArturRubets <artur.rubets@gmail.com>
2023-03-29 12:36:43 +0300

    Show data on home page

--- task-3.6/src/pages/Home.js
+++ task-3.6/src/pages/Home.js
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import Image from '../components/Image';
 
 const Home = () => {
   const postsData = useSelector((state) => state.posts.data);
@@ -10,27 +11,45 @@ const Home = () => {
       <article>
         <h2>PostList</h2>
         <ul>
-          {postsData.map((post) => (
-            <div key={post.id} className="home excerpt">
-              <div className="name">Title</div>
-              <div>{post.title}</div>
-              <div className="name">Body</div>
-              <div>{post.body}</div>
-            </div>
-          ))}
+          {postsData.map((post) => {
+            const formattedDate =
+              post.date && new Date(post.date).toLocaleDateString();
+            return (
+              <div key={post.id} className="home excerpt">
+                <div className="name">Title</div>
+                <div>{post.title}</div>
+                <div className="name">Body</div>
+                <div>{post.body}</div>
+                {post.imageUrl && (
+                  <Image className="post-img" src={post.imageUrl} alt="Post" />
+                )}
+                {post.tags?.length > 0 && (
+                  <div className="post-tags-container">
+                    {post.tags.map((tag) => (
+                      <div className="post-tag">{tag}</div>
+                    ))}
+                  </div>
+                )}
+                {post.date && <div className="post-date">{formattedDate}</div>}
+              </div>
+            );
+          })}
         </ul>
       </article>
       <article>
         <h2>TodosList</h2>
         <ul>
-          {todosData.map((post) => (
-            <div key={post.id} className="home excerpt">
-              <div className="name">Title</div>
-              <div>{post.title}</div>
-              <div className="name">Completed</div>
-              <div>{post.completed}</div>
-            </div>
-          ))}
+          {todosData.map((post) => {
+            const status = post.completed ? 'Completed' : 'Not yet';
+            return (
+              <div key={post.id} className="home excerpt">
+                <div className="name">Title</div>
+                <div>{post.title}</div>
+                <div className="name">Status</div>
+                <div>{status}</div>
+              </div>
+            );
+          })}
         </ul>
       </article>
       <article>
