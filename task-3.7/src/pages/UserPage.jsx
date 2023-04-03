9af598916e60be50d9923507c92541961e42ed4b
ArturRubets <artur.rubets@gmail.com>
2023-04-03 22:09:10 +0300

    Display details user

--- task-3.7/src/pages/UserPage.jsx
+++ task-3.7/src/pages/UserPage.jsx
@@ -1,3 +1,12 @@
+import {
+  faAddressCard,
+  faEnvelope,
+  faLocationDot,
+  faPhone,
+  faQuoteLeft,
+  faUserCircle,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { selectUserById } from '../features/users/usersSlice';
@@ -7,9 +16,109 @@ const UserPage = () => {
   const userId = parseInt(id);
   const user = useSelector((state) => selectUserById(state, userId));
   if (user) {
-    return <div>{user?.name}</div>;
+    const style = { color: 'var(--primary-500)' };
+    const {
+      name,
+      username,
+      email,
+      phone,
+      address: { street, city, zipcode } = {},
+      company: { catchPhrase } = {},
+    } = user;
+    return (
+      <section className="section">
+        <div className="row">
+          <div className="column">
+            <FontAwesomeIcon
+              className="fa-lg"
+              icon={faUserCircle}
+              style={style}
+            />
+          </div>
+          <div className="column">
+            <div className="value">{name}</div>
+            <div className="label">Name</div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            <FontAwesomeIcon
+              className="fa-lg"
+              icon={faAddressCard}
+              style={style}
+            />
+          </div>
+          <div className="column">
+            <div className="value">{username}</div>
+            <div className="label">User name</div>
+          </div>
+        </div>
+        {email && (
+          <div className="row">
+            <div className="column">
+              <FontAwesomeIcon
+                className="fa-lg"
+                icon={faEnvelope}
+                style={style}
+              />
+            </div>
+            <div className="column">
+              <div className="value">{email}</div>
+              <div className="label">Email</div>
+            </div>
+          </div>
+        )}
+        {phone && (
+          <div className="row">
+            <div className="column">
+              <FontAwesomeIcon className="fa-lg" icon={faPhone} style={style} />
+            </div>
+            <div className="column">
+              <div className="value">{phone}</div>
+              <div className="label">Mobile</div>
+            </div>
+          </div>
+        )}
+        {street && city && zipcode && (
+          <div className="row">
+            <div className="column">
+              <FontAwesomeIcon
+                className="fa-lg"
+                icon={faLocationDot}
+                style={style}
+              />
+            </div>
+            <div className="column">
+              <div className="value">
+                {street} <br /> {city}, {zipcode}
+              </div>
+              <div className="label">Work</div>
+            </div>
+          </div>
+        )}
+        {catchPhrase && (
+          <div className="row">
+            <div className="column">
+              <FontAwesomeIcon
+                className="fa-lg"
+                icon={faQuoteLeft}
+                style={style}
+              />
+            </div>
+            <div className="column">
+              <div className="value">{catchPhrase}</div>
+              <div className="label">Catch phrase</div>
+            </div>
+          </div>
+        )}
+      </section>
+    );
   } else {
-    return 'Not found';
+    return (
+      <section className="section">
+        <p className="error-not-found">Not found</p>
+      </section>
+    );
   }
 };
 export default UserPage;
