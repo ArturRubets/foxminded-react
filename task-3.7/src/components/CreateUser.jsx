ca7ed329fcffabf996b3c05431dbd5256ec7ba30
ArturRubets <artur.rubets@gmail.com>
2023-04-03 12:05:29 +0300

    Add buttons to CreateUser component

--- task-3.7/src/components/CreateUser.jsx
+++ task-3.7/src/components/CreateUser.jsx
@@ -1,8 +1,40 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { postUsers } from '../features/users/usersSlice';
 
 const CreateUser = () => {
-  const defaultNewUser = { name: '', username: '' };
-  const [newUser, setNewUser] = useState(defaultNewUser);
+  const [user, setUser] = useState({ name: '', username: '' });
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onCancel = () => {
+    navigate(-1);
+  };
+
+  const onCreateUser = () => {
+    dispatch(postUsers(user));
+    navigate(-1);
+  };
+
+  const onChangeName = (e) => {
+    setUser({ ...user, name: e.target.value });
+  };
+
+  const onChangeUsername = (e) => {
+    setUser({ ...user, username: e.target.value });
+  };
+
+  const buttons = (
+    <div className="btn-container">
+      <button className="btn btn-hipster" onClick={onCancel}>
+        Cancel
+      </button>
+      <button className="btn" onClick={onCreateUser}>
+        Create
+      </button>
+    </div>
+  );
 
   return (
     <div className="fill-new-user">
@@ -10,24 +42,17 @@ const CreateUser = () => {
       <article className="excerpt">
         <label>
           Name:
-          <input
-            type="text"
-            value={newUser.name}
-            onChange={(e) => {
-              setNewUser({ ...newUser, name: e.target.value });
-            }}
-          />
+          <input type="text" value={user.name} onChange={onChangeName} />
         </label>
         <label>
           Username:
           <input
             type="text"
-            value={newUser.username}
-            onChange={(e) => {
-              setNewUser({ ...newUser, username: e.target.value });
-            }}
+            value={user.username}
+            onChange={onChangeUsername}
           />
         </label>
+        {buttons}
       </article>
     </div>
   );
