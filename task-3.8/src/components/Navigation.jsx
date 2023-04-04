1537924cb253d3aac268898aa7d2074f4a5b41b0
ArturRubets <artur.rubets@gmail.com>
2023-04-04 20:03:59 +0300

    Remove unnecessary code

--- task-3.8/src/components/Navigation.jsx
+++ task-3.8/src/components/Navigation.jsx
@@ -3,16 +3,16 @@ import { NavLink } from 'react-router-dom';
 const Navigation = () => {
   return (
     <nav className="navbar">
-      <NavLink to="/" className="link" activeclassname="active">
+      <NavLink to="/" className="link" end>
         Home
       </NavLink>
-      <NavLink to="/posts-list" className="link" activeclassname="active">
+      <NavLink to="/posts-list" className="link">
         PostsList
       </NavLink>
-      <NavLink to="/todo-list" className="link" activeclassname="active">
+      <NavLink to="/todo-list" className="link">
         TodoList
       </NavLink>
-      <NavLink to="/user-list" className="link" activeclassname="active">
+      <NavLink to="/user-list" className="link">
         UserList
       </NavLink>
     </nav>
